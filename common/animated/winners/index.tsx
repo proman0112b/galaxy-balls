bd19439baeeb4e8fe3c9b96ec3a768a6b13bef6d
proman0112b <proman0112b@gmail.com>
2022-09-25 08:21:55 +0900

    winners modal animation

--- common/animated/winners/index.tsx
+++ common/animated/winners/index.tsx
@@ -1,12 +1,77 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import { FaFacebookF, FaSearch, FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 import { ModalProps } from '../../../hooks/useModal'
-import { Wrapper, Header, StyledModal, HeaderText, CloseButton, Content, Backdrop } from './styled'
-
+import { Wrapper, Header, StyledModal, HeaderText, CloseButton, Content, Backdrop, SearchBox, CountryWidget, WinnersHead } from './styled'
+import { WinnerTable } from '../../home/winners-section/styled'
+import type { WinnerlistStaticData } from '../../../types/winnerlist-static-data'
+import { winnerlistStaticData } from '../../../static/winnerlist-static-data'
 import { animated, useSpring, useTrail, useTransition } from 'react-spring'
 
+const WinnersTable: React.FC = () => {
+  return (
+    <WinnerTable>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Finder name</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        {winnerlistStaticData.map((data: WinnerlistStaticData, index: number) => (
+          <tr key={index}>
+            <td style={{ color: `${data.color}` }}>{data.id}</td>
+            <td style={{ color: `${data.color}` }}>{data.name}</td>
+            <td>
+              <div>
+                <FaFacebookF />
+              </div>
+              <div>
+                <AiFillInstagram />
+              </div>
+              <div>
+                <AiOutlineTwitter />
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </WinnerTable>
+  )
+}
+
 const WinnersModal: React.FC<ModalProps> = ({ isShown, hide }) => {
+  const searchBox = useTransition(isShown, {
+    config: { duration: 500 },
+    from: { opacity: 0, transform: 'translateX(-30px)' },
+    enter: { opacity: 1, transform: 'translateX(0px)' },
+  })
+  const winnerHead = useTransition(isShown, {
+    config: { duration: 500 },
+    delay: 700,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+  })
+  const winnerTable = useTransition(isShown, {
+    config: { duration: 500 },
+    delay: 1400,
+    from: { opacity: 0, transform: 'translateX(30px)' },
+    enter: { opacity: 1, transform: 'translateX(0px)' },
+  })
+
+  const countries: Array<string> = ['RU', 'BR', 'US', 'CN']
+  const trail = useTrail(countries.length, {
+    config: { mass: 5, tension: 2000, friction: 200, duration: 500 },
+    delay: 2100,
+    opacity: isShown ? 1 : 0,
+    y: isShown ? 0 : 20,
+    height: isShown ? 110 : 0,
+    from: { opacity: 0, y: 20, height: 0 },
+  })
+
   const modal = (
     <>
       <Backdrop onClick={hide} />
@@ -16,7 +81,57 @@ const WinnersModal: React.FC<ModalProps> = ({ isShown, hide }) => {
             <HeaderText>List of winners around the world</HeaderText>
             <CloseButton onClick={hide}>&#10006;</CloseButton>
           </Header>
-          <Content></Content>
+          <Content>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '40px' }}>
+              <div style={{ marginRight: '20px' }}>
+                {searchBox(style => (
+                  <animated.div style={style}>
+                    <SearchBox>
+                      <FaSearch />
+                      <input type="text" placeholder="Search Country..." />
+                    </SearchBox>
+                  </animated.div>
+                ))}
+
+                {trail.map(({ height, ...style }, key) => (
+                  <animated.div key={key} style={style}>
+                    <CountryWidget selected={key == 1}>
+                      <img src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${countries[key]}.svg`} />
+                      <div style={{ flex: '1' }}>
+                        <span>Country</span>
+                        <span>{countries[key]}</span>
+                      </div>
+                      <div>
+                        <span>Winners</span>
+                        <span>31</span>
+                      </div>
+                    </CountryWidget>
+                  </animated.div>
+                ))}
+              </div>
+
+              <div>
+                <WinnersHead>
+                  {winnerHead(style => (
+                    <animated.div style={style}>
+                      <span>Winners of</span>
+                      <span>France(44)</span>
+                    </animated.div>
+                  ))}
+                  <div>
+                    <FaChevronLeft />
+                    <FaChevronRight />
+                  </div>
+                </WinnersHead>
+
+                {winnerTable(style => (
+                  <animated.div style={style}>
+                    <WinnersTable />
+                  </animated.div>
+                ))}
+              </div>
+            </div>
+          </Content>
         </StyledModal>
       </Wrapper>
     </>

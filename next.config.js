541df91bb8061a1bf7dbc9efed011c75e57c380f
proman0112b <proman0112b@gmail.com>
2022-09-29 10:57:20 +0900

    fix: update i18n config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ['en', 'fr', 'es', 'de', 'pt', 'it'],
-    defaultLocale: 'en',
+    locales: ['default', 'en', 'fr', 'es', 'de', 'pt', 'it'],
+    defaultLocale: 'default',
   },
 }
 
